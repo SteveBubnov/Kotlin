26c2594d91ec2cc627ff4136936e91b57a9ff6cb
Stepan Bubnov <Stepan.Bubnov@aplana.com>
2019-08-15 16:43:32 +0300

    Исправлены замечания

--- src/Module1/Exercise 3.kt
+++ src/Module1/Exercise 3.kt
@@ -1,12 +1,12 @@
 package Module1
 
 fun main(){
-    val five = "5"
-    println(five.javaClass.name)
-    five.toByte()
-    five.toShort()
-    five.toInt()
-    five.toDouble()
-    five.toFloat()
-    five.toLong()
+    val five = '5'
+    val five5String = five.toString()
+    five5String.toByte()
+    five5String.toShort()
+    five5String.toInt()
+    five5String.toDouble()
+    five5String.toFloat()
+    five5String.toLong()
 }
--- src/Module2/Exercise 9.kt
+++ src/Module2/Exercise 9.kt
@@ -8,17 +8,24 @@ fun main() {
     table[2]=arrayOf("Euro","Euro","Pound sterling", "Ruble")
 
     //далее код, переварачивающий массив как сказано по заданию
-    val a = table.size
-    val b = a-1
-    val size:Array<Int> = Array(a, {0})
-    var c:Int
-    for(g in 0..b){
-        size[g]=table[g].size
-    }
-    for (i in 0..b){
-        c = size[i] - 1
-        for (h in 0..c){
-            print(table[i][h]+"\t")
+//    val a = table.size
+//    val b = a-1
+//    val size:Array<Int> = Array(a, {0})
+//    var c:Int
+//    for(g in 0..b){
+//        size[g]=table[g].size
+//    }
+//    for (i in 0..b){
+//        c = size[i] - 1
+//        for (h in 0..c){
+//            print(table[i][h]+"\t")
+//        }
+//        println()
+//    }
+
+    for (i in 0..(table[0].size -1)){
+        for (h in 0..(table.size - 1)){
+            print(table[h][i] + " ")
         }
         println()
     }
