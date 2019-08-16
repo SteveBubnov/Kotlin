74bf4e76d50c58c8d1e40e5fc0c59b89979c1af0
Stepan Bubnov <Stepan.Bubnov@aplana.com>
2019-08-16 19:03:08 +0300

    Задание 19

--- src/Module4/exercise 17-23.kt
+++ src/Module4/exercise 17-23.kt
@@ -1,15 +1,27 @@
 package Module4
 
-class Aircraft(distance:Int, fuel:Int, fuelConsumption:Any) {
-    var maxDistance: Int = distance
-    var fuelTank: Int = fuel
+fun main() {
+    val firstPlane = Boeing747(1000,20,5)
+    firstPlane.passCapacity = 500
+
+    //test
+    println("${firstPlane.maxDistance} ${firstPlane.fuelTank} ${firstPlane.fuelPerDistance} ${firstPlane.passCapacity}")
+
+}
+
+open class Aircraft(distance: Int, fuel: Int, fuelConsumption: Any) {
+    val maxDistance: Int = distance
+    val fuelTank: Int = fuel
     var fuelPerDistance = fuelConsumption
         get() = 100 * fuelTank / maxDistance
 
 }
-fun main () {
-    val cesna:Aircraft = Aircraft(1000,50,4)
-    println(cesna.fuelPerDistance)
+
+//Идея ругалась на отсутствие конструктора класса Боинг (как указывалось по примерам), поверил ей.
+class Boeing747(distance: Int, fuel: Int, fuelConsumption: Any) : Aircraft(distance, fuel, fuelConsumption) {
+    var passCapacity:Int = 0
 }
+
+
 
 
