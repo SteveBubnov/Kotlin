6c650319896cd1478867450995ffb61b8b268599
Stepan Bubnov <Stepan.Bubnov@aplana.com>
2019-08-27 19:42:23 +0300

    Задание 24

--- src/Module4/exercise 17-23.kt
+++ src/Module4/exercise 17-23.kt
@@ -1,20 +1,22 @@
 package Module4
 
+import kotlin.system.exitProcess
+var firstPlane = Boeing747(1000, 20, 5, 190)
+
+var firstCargoPlane = AirbusBeluga(5000, 50, 1, 400)
+
+var testPlane = Cessna182(1722, 10, 1, 517, 3)
+
 fun main() {
-    val firstPlane = Boeing747(1000, 20, 5)
-    firstPlane.passCapacity = 500
-    firstPlane.info()
-
-    val auto1 = Auto("1","2","3")
-    val auto2 = Auto ("Mercedes", "silver", "NH134EU")
-
-    println()
-    println(auto1.toString())
-    println(auto2.toString())
+    consoleGreets()
 }
 
 interface Passanger {
     var passCapacity: Int
+}
+
+interface Cargo {
+    var cargoCapacity: Int
 }
 
 abstract class Aircraft(protected val distance: Int, protected val fuel: Int, fuelConsumption: Any) {
@@ -26,24 +28,80 @@ abstract class Aircraft(protected val distance: Int, protected val fuel: Int, fu
 }
 
 //Идея ругалась на отсутствие конструктора класса Боинг (как указывалось по примерам), поверил ей.
-class Boeing747(distance: Int, fuel: Int, fuelConsumption: Any) : Aircraft(distance, fuel, fuelConsumption), Passanger {
-    override var passCapacity: Int = 0
+class Boeing747(distance: Int, fuel: Int, fuelConsumption: Any, pass: Int) : Aircraft(distance, fuel, fuelConsumption), Passanger {
+    override var passCapacity: Int = pass
     override fun info() {
-        println(
-            """Максимальная дальность полета: $distance
+        println("""Максимальная дальность полета: $distance
 Вместимость бака: $fuel
 Расход топлива на 100 км: $fuelPerDistance
-Вместимость пассажиров: $passCapacity"""
-        )
-
+Вместимость пассажиров: $passCapacity""")
+        reask()
     }
 }
 
-data class Auto<B,C,S>(val brand:B, val color:C, val serial:S){
+data class Auto<B, C, S>(val brand: B, val color: C, val serial: S) {
     override fun toString(): String {
         return "Car: Brand - $brand, Color - $color, Serial - $serial"
     }
 }
 
+class AirbusBeluga(distance: Int, fuel: Int, fuelConsumption: Any, cargo: Int) : Aircraft(distance, fuel, fuelConsumption), Cargo {
+    override var cargoCapacity: Int = cargo
+    override fun info() {println("""Максимальная дальность полета: $distance
+Вместимость бака: $fuel
+Расход топлива на 100 км: $fuelPerDistance
+Вместимость груза: $cargoCapacity кг""")
+        reask()}
+}
 
+class Cessna182(distance: Int, fuel: Int, fuelConsumption: Any, cargo: Int, pass: Int) :
+    Aircraft(distance, fuel, fuelConsumption), Cargo, Passanger {
+    override var cargoCapacity: Int = cargo
+    override var passCapacity: Int = pass
+    override fun info() {println("""Максимальная дальность полета: $distance
+Вместимость бака: $fuel
+Расход топлива на 100 км: $fuelPerDistance
+Вместимость груза: $cargoCapacity кг
+Вместимость пассажиров: $passCapacity""")
+        reask()
+    }
+}
+
+fun reask() {
+    println("""
+        Желаете просмотреть информацию о другой модели?
+1 - Да
+Любая другая кнопка - Нет"""
+    )
+    val h = readLine()
+    if (h == "1") consoleGreets()
+    else {
+        exitProcess(0)
+    }
+}
+fun consoleGreets() {
+    println(
+        """по какому самолету желаете просмотреть информацию?
+1. Boeing747
+2. AirbusBeluga
+3. Cessna182""")
+    val i = readLine()
+    when (i) {
+        "1" -> println(firstPlane.info())
+        "2" -> println(firstCargoPlane.info())
+        "3" -> println(testPlane.info())
+        else -> {
+            println(
+                """Выбор не распознан, повторить попытку?
+1 - Да
+Любая другая кнопка - Нет"""
+            )
+            val h = readLine()
+            if (h == "1") consoleGreets()
+            else {
+                exitProcess(0)
+            }
+        }
+    }
+}
 
